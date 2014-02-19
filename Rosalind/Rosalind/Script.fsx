7686783b80f9be75bd3dde06a7781170669a771f
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-02-18 20:01:28 -0800

    Problem 4

--- Rosalind/Rosalind/Script.fsx
+++ Rosalind/Rosalind/Script.fsx
@@ -36,7 +36,13 @@ let problem_3 () =
     let output = new System.String (input |> String.map dnaComplement |> Seq.toArray |> Array.rev)
     output = "ACCGGGTTTT"
 
-// TODO PROBLEM 4
+// http://rosalind.info/problems/fib/
+let problem_4 () =
+    
+    let n,k = 5,3
+    let next (o,n) = (o+n,o*k)
+    let pop = Seq.unfold (fun (o,n) -> Some(o+n,next(o,n))) (0,1)
+    (pop |> Seq.nth (n-1)) = 19
 
 // http://rosalind.info/problems/gc/
 let problem_5 () =
