3c8257a68c2b242ddf428c1388835953a78a5885
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-02-27 19:21:35 -0800

    FIB (prob 4) complete

--- Racket/004-FIB.rkt
+++ Racket/004-FIB.rkt
@@ -1,15 +1,19 @@
 ; http://rosalind.info/problems/fib/
 #lang racket
 ; compute next population pair
-(define (next pop g)
+; pop = old,new
+(define (next pop growth)
   (match pop
-    [(cons curr prev) (cons (+ curr (* prev g)) curr)]))
+    [(cons old new) (cons (+ new old) (* old growth))]))
 
 (define (total pop)
   (match pop
-    [(cons curr prev) (+ curr prev)]))
+    [(cons old new) (+ old new)]))
 
-(define (grow pop rate iters)
-  (if (> iters 0)
-      (grow (next pop rate) rate (- iters 1))
+(define (grow pop iters growth)
+  (if (> iters 1)
+      (grow (next pop growth) (- iters 1) growth)
       (total pop)))
+
+; check against example
+(equal? (grow (cons 0 1) 5 3) 19)
